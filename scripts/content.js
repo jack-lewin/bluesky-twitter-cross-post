d61f312c78863b6a192ccfd8855cb759ad486006
Jack Lewin <14926880+jack-lewin@users.noreply.github.com>
2024-11-16 22:49:31 +0000

    Debug - image upload

--- scripts/content.js
+++ scripts/content.js
@@ -2,12 +2,19 @@ async function receiveBlueskyTwitterCrossPost(msg, sender, sendResponse) {
   if (msg.type !== "bluesky_twitter_cross_post") {
     return;
   }
+  console.log("Message received:");
+  console.log(msg);
 
   const input = document.querySelector("[contenteditable='true']");
   // const input = document.querySelector("[role='textbox']");
 
   input.focus();
   document.execCommand('insertText', false, msg.post.text);
+
+  // TODO - trigger image upload
+
+  // Don't post tweets while debugging
+  return;
 
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
